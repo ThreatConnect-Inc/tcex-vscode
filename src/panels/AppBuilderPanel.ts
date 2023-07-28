396264c7dc98f40dfaf3a9af12985daece8d8e93
Chris Blades <cblades@threatconnect.com>
2023-07-28 17:32:25 -0400

    update icon path

--- src/panels/AppBuilderPanel.ts
+++ src/panels/AppBuilderPanel.ts
@@ -1,11 +1,10 @@
-import { Disposable, Webview, WebviewPanel, window, Uri, ViewColumn } from "vscode";
+import { Disposable, Webview, WebviewPanel, window, Uri, ViewColumn } from 'vscode';
 import * as vscode from 'vscode';
-import { getUri } from "../utilities/getUri";
-import { getNonce } from "../utilities/getNonce";
-import { AppSpecObserver } from "../appSpecWatcher";
+import { getUri } from '../utilities/getUri';
+import { getNonce } from '../utilities/getNonce';
+import { AppSpecObserver } from '../appSpecWatcher';
 
-const outputTypes = ["String", "StringArray", "KeyValue", "KeyValueArray", "TCEntity", "TCEntityArray"];
-
+const outputTypes = ['String', 'StringArray', 'KeyValue', 'KeyValueArray', 'TCEntity', 'TCEntityArray'];
 
 /**
  * This class manages the state and behavior of HelloWorld webview panels.
@@ -18,137 +17,134 @@ const outputTypes = ["String", "StringArray", "KeyValue", "KeyValueArray", "TCEn
  * - Setting message listeners so data can be passed between the webview and extension
  */
 export class AppBuilderPanel implements AppSpecObserver {
-    public static currentPanel: AppBuilderPanel | undefined;
-    private readonly _panel: WebviewPanel;
-    private _disposables: Disposable[] = [];
-    private _appYaml: any | undefined;
+  public static currentPanel: AppBuilderPanel | undefined;
+  private readonly _panel: WebviewPanel;
+  private _disposables: Disposable[] = [];
+  private _appYaml: any | undefined;
 
-    /**
-     * The HelloWorldPanel class private constructor (called only from the render method).
-     *
-     * @param panel A reference to the webview panel
-     * @param extensionUri The URI of the directory containing the extension
-     */
-    private constructor(panel: WebviewPanel, extensionUri: Uri) {
-        this._panel = panel;
+  /**
+   * The HelloWorldPanel class private constructor (called only from the render method).
+   *
+   * @param panel A reference to the webview panel
+   * @param extensionUri The URI of the directory containing the extension
+   */
+  private constructor(panel: WebviewPanel, extensionUri: Uri) {
+    this._panel = panel;
 
-        // Set an event listener to listen for when the panel is disposed (i.e. when the user closes
-        // the panel or when the panel is closed programmatically)
-        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    // Set an event listener to listen for when the panel is disposed (i.e. when the user closes
+    // the panel or when the panel is closed programmatically)
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
 
-        this._panel.iconPath = Uri.joinPath(extensionUri, 'assets', 'icon.png');
+    this._panel.iconPath = Uri.joinPath(extensionUri, 'icon.png');
 
-        // Set the HTML content for the webview panel
-        this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
+    // Set the HTML content for the webview panel
+    this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
 
+    // Set an event listener to listen for messages passed from the webview context
+    this._setWebviewMessageListener(this._panel.webview);
+  }
+  onAppSpecChange(appSpec: any): void {
+    this.appYaml = appSpec;
+  }
 
-        // Set an event listener to listen for messages passed from the webview context
-        this._setWebviewMessageListener(this._panel.webview);
-    }
-    onAppSpecChange(appSpec: any): void {
-        this.appYaml = appSpec;
-    }
+  private get appYaml(): any {
+    return this._appYaml;
+  }
 
+  private set appYaml(ij: any) {
+    this._appYaml = ij;
+    this._panel.webview.postMessage({ command: 'appConfig', appSpec: this.appYaml });
+  }
 
-    private get appYaml(): any {
-        return this._appYaml;
-    }
+  get webview(): Webview {
+    return this._panel.webview;
+  }
 
-    private set appYaml(ij: any) {
-        this._appYaml = ij;
-        this._panel.webview.postMessage({ command: "appConfig", appSpec: this.appYaml });
-    }
+  public static dispose() {
+    AppBuilderPanel.currentPanel?.dispose();
+    AppBuilderPanel.currentPanel = undefined;
+  }
 
-    get webview(): Webview {
-        return this._panel.webview;
-    }
-
-    public static dispose() {
-        AppBuilderPanel.currentPanel?.dispose();
-        AppBuilderPanel.currentPanel = undefined;
-    }
-
-    /**
-     * Renders the current webview panel if it exists otherwise a new webview panel
-     * will be created and displayed.
-     *
-     * @param extensionUri The URI of the directory containing the extension.
-     */
-    public static render(extensionUri: Uri): AppBuilderPanel {
-        if (AppBuilderPanel.currentPanel) {
-            // If the webview panel already exists reveal it
-            AppBuilderPanel.currentPanel._panel.reveal(ViewColumn.Two);
-        } else {
-            // If a webview panel does not already exist create and show a new one
-            const panel = window.createWebviewPanel(
-                // Panel view type
-                "showHelloWorld",
-                // Panel title
-                "TcEx App Framework",
-                // The editor column the panel should be displayed in
-                ViewColumn.Two,
-                // Extra panel configurations
-                {
-                    // Enable JavaScript in the webview
-                    enableScripts: true,
-                    // Restrict the webview to only load resources from the `out` and `webview-ui/build` directories
-                    localResourceRoots: [
-                        Uri.joinPath(extensionUri, "out"),
-                        Uri.joinPath(extensionUri, "webview-ui/build"),
-                        Uri.joinPath(extensionUri, "webview-ui/build/assets"),
-                        Uri.joinPath(extensionUri, "assets")
-                    ],
-                }
-            );
-
-            AppBuilderPanel.currentPanel = new AppBuilderPanel(panel, extensionUri);
+  /**
+   * Renders the current webview panel if it exists otherwise a new webview panel
+   * will be created and displayed.
+   *
+   * @param extensionUri The URI of the directory containing the extension.
+   */
+  public static render(extensionUri: Uri): AppBuilderPanel {
+    if (AppBuilderPanel.currentPanel) {
+      // If the webview panel already exists reveal it
+      AppBuilderPanel.currentPanel._panel.reveal(ViewColumn.Two);
+    } else {
+      // If a webview panel does not already exist create and show a new one
+      const panel = window.createWebviewPanel(
+        // Panel view type
+        'showHelloWorld',
+        // Panel title
+        'TcEx App Framework',
+        // The editor column the panel should be displayed in
+        ViewColumn.Two,
+        // Extra panel configurations
+        {
+          // Enable JavaScript in the webview
+          enableScripts: true,
+          // Restrict the webview to only load resources from the `out` and `webview-ui/build` directories
+          localResourceRoots: [
+            Uri.joinPath(extensionUri, 'out'),
+            Uri.joinPath(extensionUri, 'webview-ui/build'),
+            Uri.joinPath(extensionUri, 'webview-ui/build/assets'),
+            Uri.joinPath(extensionUri, 'assets'),
+          ],
         }
+      );
 
-        return AppBuilderPanel.currentPanel;
+      AppBuilderPanel.currentPanel = new AppBuilderPanel(panel, extensionUri);
     }
 
-    /**
-     * Cleans up and disposes of webview resources when the webview panel is closed.
-     */
-    public dispose() {
-        AppBuilderPanel.currentPanel = undefined;
+    return AppBuilderPanel.currentPanel;
+  }
 
-        // Dispose of the current webview panel
-        this._panel.dispose();
+  /**
+   * Cleans up and disposes of webview resources when the webview panel is closed.
+   */
+  public dispose() {
+    AppBuilderPanel.currentPanel = undefined;
 
-        // Dispose of all disposables (i.e. commands) for the current webview panel
-        while (this._disposables.length) {
-            const disposable = this._disposables.pop();
-            if (disposable) {
-                disposable.dispose();
-            }
-        }
+    // Dispose of the current webview panel
+    this._panel.dispose();
+
+    // Dispose of all disposables (i.e. commands) for the current webview panel
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
     }
+  }
 
+  /**
+   * Defines and returns the HTML that should be rendered within the webview panel.
+   *
+   * @remarks This is also the place where references to the Angular webview build files
+   * are created and inserted into the webview HTML.
+   *
+   * @param webview A reference to the extension webview
+   * @param extensionUri The URI of the directory containing the extension
+   * @returns A template string literal containing the HTML that should be
+   * rendered within the webview panel
+   */
+  private _getWebviewContent(webview: Webview, extensionUri: Uri) {
+    // The CSS file from the Angular build output
+    const stylesUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'styles.css']);
+    // The JS files from the Angular build output
+    const runtimeUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'runtime.js']);
+    const polyfillsUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'polyfills.js']);
+    const scriptUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'main.js']);
+    const baseUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'assets']);
+    const nonce = getNonce();
 
-    /**
-     * Defines and returns the HTML that should be rendered within the webview panel.
-     *
-     * @remarks This is also the place where references to the Angular webview build files
-     * are created and inserted into the webview HTML.
-     *
-     * @param webview A reference to the extension webview
-     * @param extensionUri The URI of the directory containing the extension
-     * @returns A template string literal containing the HTML that should be
-     * rendered within the webview panel
-     */
-    private _getWebviewContent(webview: Webview, extensionUri: Uri) {
-        // The CSS file from the Angular build output
-        const stylesUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'styles.css']);
-        // The JS files from the Angular build output
-        const runtimeUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'runtime.js']);
-        const polyfillsUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'polyfills.js']);
-        const scriptUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'main.js']);
-        const baseUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'assets']);
-        const nonce = getNonce();
-
-        // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
-        return /*html*/ `
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/ `
       <!DOCTYPE html>
       <html lang="en">
         <head>
@@ -170,59 +166,60 @@ export class AppBuilderPanel implements AppSpecObserver {
         </body>
       </html>
     `;
-    }
+  }
 
-    /**
-     * Sets up an event listener to listen for messages passed from the webview context and
-     * executes code based on the message that is recieved.
-     *
-     * @param webview A reference to the extension webview
-     * @param context A reference to the extension context
-     */
-    private _setWebviewMessageListener(webview: Webview) {
-        webview.onDidReceiveMessage(
-            (message: any) => {
-                const command = message.command;
+  /**
+   * Sets up an event listener to listen for messages passed from the webview context and
+   * executes code based on the message that is recieved.
+   *
+   * @param webview A reference to the extension webview
+   * @param context A reference to the extension context
+   */
+  private _setWebviewMessageListener(webview: Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
 
-                switch (command) {
-                    case 'addOutput':
-                        vscode.window.showInputBox({ prompt: "Output name" }).then((outputName) => {
-                            vscode.window.showQuickPick(outputTypes).then((outputType) => {
-                                if (outputName && outputType) {
-                                    this.appYaml.playbook.outputVariables.push({
-                                        name: outputName,
-                                        type: outputType
-                                    });
-                                    vscode.workspace.fs.writeFile(this._appYaml, Buffer.from(JSON.stringify(this.appYaml, null, 4))).then(() => {
-                                        this.webview.postMessage({ command: "appConfig", installJson: this.appYaml });
-                                    });
-                                }
-                            });
-                        });
-                        break;
-                    case 'hello':
-                        this.webview.postMessage({ command: "appConfig", appSpec: this.appYaml });
-                        break;
-                    case 'sendToClipboard':
-                        if (message.type === 'input') {
-                            const name = message.value.name || message.value.label.toLowerCase().replace(/ /g, '_');
-                            const accessor = message.value.encrypt ? '.value' : '';
-                            const output = `self.in_.${name}${accessor}`;
-                            vscode.env.clipboard.writeText(output).then(() => {
-                                vscode.window.showInformationMessage(`Code copied to clipboard: ${output}`);
-                            });
-                        } else if (message.type === 'output') {
-                            const name = message.value.name;
-                            const output = `self.playbook.create.variable('${name}', ...)`;
-                            vscode.env.clipboard.writeText(output).then(() => {
-                                vscode.window.showInformationMessage(`Code copied to clipboard: ${output}`);
-                            });
-                        }
-
+        switch (command) {
+          case 'addOutput':
+            vscode.window.showInputBox({ prompt: 'Output name' }).then((outputName) => {
+              vscode.window.showQuickPick(outputTypes).then((outputType) => {
+                if (outputName && outputType) {
+                  this.appYaml.playbook.outputVariables.push({
+                    name: outputName,
+                    type: outputType,
+                  });
+                  vscode.workspace.fs
+                    .writeFile(this._appYaml, Buffer.from(JSON.stringify(this.appYaml, null, 4)))
+                    .then(() => {
+                      this.webview.postMessage({ command: 'appConfig', installJson: this.appYaml });
+                    });
                 }
-            },
-            undefined,
-            this._disposables
-        );
-    }
+              });
+            });
+            break;
+          case 'hello':
+            this.webview.postMessage({ command: 'appConfig', appSpec: this.appYaml });
+            break;
+          case 'sendToClipboard':
+            if (message.type === 'input') {
+              const name = message.value.name || message.value.label.toLowerCase().replace(/ /g, '_');
+              const accessor = message.value.encrypt ? '.value' : '';
+              const output = `self.in_.${name}${accessor}`;
+              vscode.env.clipboard.writeText(output).then(() => {
+                vscode.window.showInformationMessage(`Code copied to clipboard: ${output}`);
+              });
+            } else if (message.type === 'output') {
+              const name = message.value.name;
+              const output = `self.playbook.create.variable('${name}', ...)`;
+              vscode.env.clipboard.writeText(output).then(() => {
+                vscode.window.showInformationMessage(`Code copied to clipboard: ${output}`);
+              });
+            }
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
 }
