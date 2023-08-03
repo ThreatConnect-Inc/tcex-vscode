ea0014ec8a1bf15d899509839f73362604c18a54
Chris Blades <cblades@threatconnect.com>
2023-08-03 16:25:59 -0400

    Fix issue with terminal re-use

---
+++ release_notes.md
@@ -0,0 +1,9 @@
+# Release Notes
+
+### 1.0.1
+
+-   [Terminal] Fix issue with terminal reuse after running a tcex command.
+
+### 1.0.0
+
+-   Initial Release
--- src/terminalManager.ts
+++ src/terminalManager.ts
@@ -1,38 +1,36 @@
 import * as vscode from 'vscode';
 
-
 export class TerminalManager {
-    private _terminal?: vscode.Terminal;
-    private options: vscode.TerminalOptions;
+  private _terminal?: vscode.Terminal;
+  private options: vscode.TerminalOptions;
 
-    constructor(options: vscode.TerminalOptions) {
-        this.options = options;
+  constructor(options: vscode.TerminalOptions) {
+    this.options = options;
 
-        vscode.window.onDidCloseTerminal((terminal) => {
-            if (this._terminal === terminal) {
-                this._terminal = undefined;
-            }
-        });
-    }
-
-    get terminal(): Thenable<vscode.Terminal> {
-        if (!this._terminal) {
-            this._terminal = vscode.window.createTerminal(this.options);
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve(this._terminal!);
-                }, 1500);
-
-            });
-        }
-
-        return new Promise((resolve) => this._terminal!);
-    }
-
-    public dispose() {
-        if (this._terminal) {
-            this._terminal.dispose();
-        }
+    vscode.window.onDidCloseTerminal((terminal) => {
+      if (this._terminal === terminal) {
         this._terminal = undefined;
+      }
+    });
+  }
+
+  get terminal(): Thenable<vscode.Terminal> {
+    if (!this._terminal) {
+      this._terminal = vscode.window.createTerminal(this.options);
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(this._terminal!);
+        }, 1500);
+      });
     }
+
+    return new Promise((resolve) => resolve(this._terminal!));
+  }
+
+  public dispose() {
+    if (this._terminal) {
+      this._terminal.dispose();
+    }
+    this._terminal = undefined;
+  }
 }
